805aee3ea9419ec7839c2a6e14c0b12c801f94e6
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-21 19:40:50 -0700

    added url submitter page, and random string generator

--- express_server.js
+++ express_server.js
@@ -3,7 +3,14 @@ const res = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8); 
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -39,5 +46,10 @@ app.get("/urls/:shortURL", (req, res) => {
   console.log("req params", req.params.shortURL)
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
