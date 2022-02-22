9412d43a44aae5659de246e522ad2649eef92d0a
Manuel Zuniga <zuniga@ualberta.ca>
2022-02-21 18:54:22 -0700

    added a url new page for a form to collect urls

--- express_server.js
+++ express_server.js
@@ -31,8 +31,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   console.log("req params", req.params.shortURL)
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
